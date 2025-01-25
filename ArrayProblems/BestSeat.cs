b8a9004e27c4124483241fa7bd62b080eb600dbc
Hussain Kharodawala <hussain.abbas52@gmail.com>
2025-01-25 23:13:26 +0530

    ftr: best seat

---
+++ ArrayProblems/BestSeat.cs
@@ -0,0 +1,31 @@
+﻿namespace ArrayProblems;
+public class BestSeat
+{
+    public Program MyProgram => new Program();
+    public class Program
+    {
+        public int BestSeat(int[] seats)
+        {
+            var bestSeat = -1;
+            var left = 0;
+            var maxSpace = 0;
+            while (left < seats.Length)
+            {
+                var right = left + 1;
+                while (right < seats.Length && seats[right] == 0)
+                {
+                    right++;
+                }
+
+                var availableSpace = right - left - 1;
+                if (availableSpace > maxSpace)
+                {
+                    maxSpace = availableSpace;
+                    bestSeat = (left + right) / 2;
+                }
+                left = right;
+            }
+            return bestSeat;
+        }
+    }
+}
---
+++ DsaUnitTests/Arrays/BestSeatTests.cs
@@ -0,0 +1,21 @@
+﻿using ArrayProblems;
+
+namespace DsaUnitTests.Arrays;
+
+public class BestSeatTests
+{
+    [Fact]
+    public void BestSeat_Should_Return_Correct_Position()
+    {
+        // Arrange
+        var input = new int[] { 1, 0, 1, 0, 0, 0, 1 };
+        var expected = 4;
+        var bestSeat = new BestSeat();
+
+        // Act
+        var result = bestSeat.MyProgram.BestSeat(input);
+
+        // Assert
+        Assert.True(result == expected);
+    }
+}
