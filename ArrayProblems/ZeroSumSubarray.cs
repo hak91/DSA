fee35adeee1ec577c5e810477001cc911c07beb8
Hussain Kharodawala <hussain.abbas52@gmail.com>
2025-03-16 21:58:46 +0530

    ftr: zero sum sub array

---
+++ ArrayProblems/ZeroSumSubarray.cs
@@ -0,0 +1,28 @@
+﻿namespace ArrayProblems;
+
+public class ZeroSumSubarray
+{
+    public class Program
+    {
+        public bool ZeroSumSubarray(int[] nums)
+        {
+            var sumsCount = new Dictionary<int, int>();
+            var currentSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                currentSum += nums[i];
+                if (!sumsCount.ContainsKey(currentSum))
+                {
+                    sumsCount[currentSum] = 0;
+                }
+                sumsCount[currentSum]++;
+                if (sumsCount[currentSum] > 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
