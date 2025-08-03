98c4bb10640f910467c464989c75041292bb0c35
Hussain Kharodawala <hussain.abbas52@gmail.com>
2025-08-03 22:18:51 +0530

    ftr: dynamic program

---
+++ DsaUnitTests/DynamicProgramming/MaxSubsetSumNoAdjacentTests.cs
@@ -0,0 +1,32 @@
+﻿using DynamicProgramming;
+using FluentAssertions;
+
+namespace DsaUnitTests.DynamicProgramming;
+public class MaxSubsetSumNoAdjacentTests
+{
+    [Fact]
+    public void MaxSubsetSumNoAdjacent_Should_Return_Expected_Value()
+    {
+        // Arrange
+        int[] input = [75, 105, 120, 75, 90, 135];
+
+        //Act
+        var result = DynamicProgrammingQuestions.MaxSubsetSumNoAdjacent(input);
+
+        //Assert
+        result.Should().Be(330);
+    }
+
+    [Fact]
+    public void MaxSubsetSumNoAdjacent_Should_Return_Zero_For_Empty()
+    {
+        // Arrange
+        int[] input = [];
+
+        //Act
+        var result = DynamicProgrammingQuestions.MaxSubsetSumNoAdjacent(input);
+
+        //Assert
+        result.Should().Be(0);
+    }
+}
---
+++ DynamicProgramming/DynamicProgrammingQuestions.cs
@@ -0,0 +1,39 @@
+﻿namespace DynamicProgramming;
+public class DynamicProgrammingQuestions
+{
+    public static int MaxSubsetSumNoAdjacent(int[] array)
+    {
+        if (array.Length == 0) return 0;
+
+        var sumUntilMinusTwo = (array[0], true);
+
+        if (array.Length == 1)
+        {
+            return sumUntilMinusTwo.Item1;
+        }
+
+        var sumUntilMinusOne = array[1] > array[0] ? (array[1], true) : (array[0], false);
+
+        if (array.Length == 2)
+        {
+            return sumUntilMinusOne.Item1;
+        }
+
+        for (int i = 2; i < array.Length; i++)
+        {
+            var currentSum1 = sumUntilMinusTwo.Item1 + array[i];
+            var currentSum2 = sumUntilMinusOne.Item2 ? sumUntilMinusOne.Item1 : sumUntilMinusOne.Item1 + array[i];
+
+            if (currentSum1 >= currentSum2)
+            {
+                sumUntilMinusTwo = sumUntilMinusOne;
+                sumUntilMinusOne = (currentSum1, true);
+                continue;
+            }
+            sumUntilMinusTwo = sumUntilMinusOne;
+            sumUntilMinusOne = (currentSum2, false);
+        }
+
+        return sumUntilMinusTwo.Item1 > sumUntilMinusOne.Item1 ? sumUntilMinusTwo.Item1 : sumUntilMinusOne.Item1;
+    }
+}
