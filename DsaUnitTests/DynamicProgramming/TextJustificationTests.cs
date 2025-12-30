2bef728819b76cca06430d14af8544b53733001b
Hussain Kharodawala <hussain.abbas52@gmail.com>
2025-12-30 10:29:51 +0530

    Add TextJustification class with badness calculation and tests
    Implemented a recursive text justification algorithm in the new TextJustification class, including methods to compute line badness and overall minimum badness. Added a unit test using xUnit and FluentAssertions to verify output of the Run method.

---
+++ DsaUnitTests/DynamicProgramming/TextJustificationTests.cs
@@ -0,0 +1,25 @@
+﻿using DynamicProgramming;
+using FluentAssertions;
+
+namespace DsaUnitTests.DynamicProgramming;
+public class TextJustificationTests
+{
+    [Fact]
+    public void Run_ShouldPrintResult_ForGivenWordsAndLineWidth()
+    {
+        // Arrange
+        var words = new[] { "This", "is", "a", "test" };
+        int lineWidth = 10;
+        var justification = new TextJustification();
+
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
+
+        // Act
+        justification.Run(words, lineWidth);
+
+        // Assert
+        var output = sw.ToString();
+        output.Should().Contain("Result:");
+    }
+}
---
+++ DynamicProgramming/TextJustification.cs
@@ -0,0 +1,40 @@
+﻿namespace DynamicProgramming;
+public class TextJustification
+{
+    public void Run(string[] words, int lineWidth)
+    {
+        var result = this.Badness(words, lineWidth, 0);
+
+        Console.WriteLine($"Result: {result}");
+    }
+
+    private int Badness(IEnumerable<string> words, int lineWidth, int i)
+    {
+        if (i == words.Count() - 1)
+        {
+            return 0;
+        }
+        int minimum = int.MaxValue;
+        for (int j = i + 1; j < words.Count(); j++)
+        {
+            var badness = this.BadnessFor(words.Skip(i).Take(j), lineWidth);
+            var badnessOfSuffix = this.Badness(words, lineWidth, j);
+            if (badness + badnessOfSuffix <= minimum)
+            {
+                minimum = badness + badnessOfSuffix;
+            }
+        }
+        return minimum;
+    }
+
+    private int BadnessFor(IEnumerable<string> words, int lineWidth)
+    {
+        var length = words.Sum(w => w.Length) + words.Count() - 1;
+
+        if (length > lineWidth)
+        {
+            return int.MaxValue;
+        }
+        return (int)Math.Pow((lineWidth - length), 3);
+    }
+}
